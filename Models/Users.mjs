5b566ffca29843952c1c3d7bf0499713465cb6c2
Marcos Gomez <adayintexas@gmail.com>
2020-01-10 20:01:23 +0100

    update user model with exercises

--- Models/Users.mjs
+++ Models/Users.mjs
@@ -2,8 +2,15 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+const Exercise = new Schema({
+  description: String,
+  duration: Number,
+  date: Date
+});
+
 const Users = new Schema({
-  username: String
+  username: String,
+  exercises: [Exercise]
 });
 
 const UserModel = mongoose.model("Users", Users);

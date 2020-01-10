b5cd1c05666a7c15ca5cb254e6eb15447f158e75
Marcos Gomez <adayintexas@gmail.com>
2020-01-10 19:35:39 +0100

    modify Users model

--- Models/Users.mjs
+++ Models/Users.mjs
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Users = new Schema({
-  userName: String,
-  userId: String
+  username: String
 });
 
 const UserModel = mongoose.model("Users", Users);
